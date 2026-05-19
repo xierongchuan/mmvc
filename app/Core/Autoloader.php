2936ff2f6294f943229095f64ae9282f71241dd2
黑鱼 <xierongchuan@tutamail.com>
2026-05-19 22:33:39 +0500

    feat: implement PSR-4 autoloader in the Autoloader class

--- app/Core/Autoloader.php
+++ app/Core/Autoloader.php
@@ -1,1 +1,19 @@
 <?php
+namespace App\Core;
+
+// Автолоадер с PSR-4
+class Autoloader
+{
+    public static function register()
+    {
+        spl_autoload_register(function (string $class): void {
+            $prefix = 'App\\';
+            if (str_starts_with($class, $prefix) === false) return;
+
+            $relativeClass = substr($class, strlen($prefix));
+            $file = __DIR__ . '/../' . str_replace('\\', '/', $relativeClass) . '.php';
+            
+            if (is_file($file)) require $file;
+        });
+    }
+}
